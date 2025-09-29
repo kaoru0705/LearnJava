3e1aa6bd047a923337d7e425d143ffd781a9e97a
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-09-29 16:51:06 +0900

    type casting

--- P0929_4/src/P8.java
+++ P0929_4/src/P8.java
@@ -11,6 +11,6 @@ public class P8 {
 
         // a = b; // Syntax Error mismatch from int to byte 정보의 손실이 발생하기 때문에 큰 범위에서 작음 범위로 못 들어감.
         b = a; // implicit type casting(묵시적 형변환), promotion
-        a = (byte)b; // explicit type casting(명시적 형변환) - 확신이 들지 않으면 절대 좋은 코드가 아니다.
+        a = (byte)b; // explicit type casting(명시적 형변환) - 논리적 확신이 들 때만 사용. 절대 좋은 코드가 아니다.
     }
 }
