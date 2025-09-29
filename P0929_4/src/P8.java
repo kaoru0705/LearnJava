73d980ba16054d7e5b06bfca1fc4c66a6dbb9f45
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-09-29 16:53:53 +0900

    double type casting

--- P0929_4/src/P8.java
+++ P0929_4/src/P8.java
@@ -12,5 +12,9 @@ public class P8 {
         // a = b; // Syntax Error mismatch from int to byte 정보의 손실이 발생하기 때문에 큰 범위에서 작음 범위로 못 들어감.
         b = a; // implicit type casting(묵시적 형변환), promotion
         a = (byte)b; // explicit type casting(명시적 형변환) - 논리적 확신이 들 때만 사용. 절대 좋은 코드가 아니다.
+
+        // double v1 = 10; // type mismatch promotion으로 허용이지만 정수 리터럴을 실수에 넣을 필요가 없다.
+        double v1 = 10.0;
+        int v2 = (int)30.0;
     }
 }
