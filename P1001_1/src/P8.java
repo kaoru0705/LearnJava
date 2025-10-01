64a6d7de16644c5bb810eeb93f81abe70a10c030
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-10-01 14:28:01 +0900

    Update P8.java
    while문은 continue쓰기 껄끄러울 수도 있다.

--- P1001_1/src/P8.java
+++ P1001_1/src/P8.java
@@ -4,12 +4,22 @@ public class P8 {
 
         int sum = 0;
         for(int i = 1; i <= 10; i++) {
-            if(i%3 == 0){
-                continue;
-            }
+            if(i%3 == 0) continue;
             sum += i;
         }
-
         System.out.println(sum);
+
+        // while
+        int sum2 = 0;
+        int i2 = 1;
+        while (i2 <= 10) {
+            if(i2 % 3 == 0) {
+                i2++;
+                continue;
+            }
+            sum2 += i2;
+            i2++;
+        }
+        System.out.println(sum2);
     }
 }
