7e6e5b189e086c8ede0d5f42a8cc49b36241df87
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-10-01 17:49:34 +0900

    Casino
    돈을 배팅하고 5가지 게임을 선택해서 할 수 있는 프로그램이다. 그 중 홀짝과 업앤다운을 구현했다.
    문자열로 바꾸기 위해 +"" 을 추가하는 건 신박하다.

---
+++ P1001_1/src/Casino.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+public class Casino {
+    public static void main(String[] args) {
+        // 카지노
+        // 초기 자본은 1000원을 준다.
+        // 총 n개의 게임 중 골라서 게임을 진행 가능. 배팅이 가능하다.
+        // 홀짝, 가위바위보, 단순 로또, 업앤다운
+        // 퇴장을 하거나, 모든 돈을 잃으면 게임이 끝난다.
+        int coin = 1000;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("***********************************");
+        System.out.println("*               중앙 카지노             *");
+        while(true) {
+            System.out.println("다음 중 진행할 게임을 선택하세요.");
+            System.out.println("1. 홀짝");
+            System.out.println("2. 가위바위보");
+            System.out.println("3. 로또");
+            System.out.println("4. 업앤다운");
+            System.out.println("0. 끝내기");
+            System.out.print("선택 > ");
+            String command = scanner.nextLine();
+            // 왜 0과 1을 else if 잇지 않았나? 개념이 달라서 구분한다.
+            if (command.equals("0")) {
+                break;
+            }
+            if (command.equals("1")) {
+                System.out.println("------- 홀짝 게임 -------");
+                System.out.print("배팅액 입력(남은 돈:"+coin+") > ");
+                int bet = Integer.parseInt(scanner.nextLine());
+                
+                coin -= bet;
+
+                System.out.println("홀 혹은 짝을 선택해주세요 > ");
+                System.out.println("1. 홀");
+                System.out.println("2. 짝");
+                System.out.print("입력 > ");
+                String even = scanner.nextLine();
+                
+                int result = (int)(Math.random() * 2) + 1;
+                String strResult = result + "";
+
+                String textResultEven = result == 1 ? "홀" : "짝";
+                System.out.println(textResultEven + "이 나왔습니다.");
+
+                if(strResult.equals(even)) {
+                    System.out.println("당신은 이겼습니다.");
+                    coin += bet * 2;
+                } else {
+                    System.out.println("당신은 졌습니다.");
+                }
+                
+            } else if (command.equals("2")) {
+                break;
+            } else if (command.equals("3")) {
+                break;
+            } else if (command.equals("4")) {
+                System.out.println("------- 업앤다운 -------");
+                System.out.println("숫자는 0~46사이의 값이 있습니다.");
+                System.out.println("총 시도 횟수는 5입니다.");
+
+                System.out.print("베팅금액 입력(가진돈: "+coin+") > ");
+                int bet = Integer.parseInt(scanner.nextLine());
+                coin -= bet;
+
+                int targetNumber = (int)(Math.random() * 47);
+                boolean isRight = false;
+                for(int i = 5; i > 0; i--) {
+                    System.out.print("예상 값을 입력하세요. ("+i+"회 남음) > ");
+                    int guessNumber = Integer.parseInt(scanner.nextLine());
+                    if(targetNumber > guessNumber) {
+                        System.out.println("업!");
+                    } else if(targetNumber < guessNumber) {
+                        System.out.println("다운!");
+                    } else {
+                        System.out.println("정답입니다!!! 승리하셨습니다.");
+                        coin += bet * 3;
+                        isRight = true;
+                        break;
+                    }
+                }
+                if (!isRight) {
+                    System.out.println("정답을 맞추지 못하셨습니다. 패배");
+                }
+
+            } else {
+                System.out.println("잘못된 값을 입력하셨습니다. 다시 시도해주세요.");
+            }
+
+            // ... 게임 종료 시점
+            if (coin <= 0) {
+                System.out.println("모든 돈을 잃었습니다.");
+                break;
+            }
+
+            System.out.println("계속 진행하시려면 enter를 입력해주세요");
+            scanner.nextLine();
+        }
+        
+        System.out.println("이용해주셔서 감사합니다. 게임을 종료합니다.");
+        
+
+    }
+}
