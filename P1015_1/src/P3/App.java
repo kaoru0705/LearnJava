a750298d99fe4c56963cb211efb43765e14c6f11
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-10-15 10:56:20 +0900

    Update App.java

--- P1015_1/src/P3/App.java
+++ P1015_1/src/P3/App.java
@@ -1,12 +1,39 @@
 package P3;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        
+        Calculator calculator = new Calculator();
+
+        int inputStart = 77;
+        int inputEnd = 1500;
+        System.out.println(calculator.SumFromTo(inputStart, inputEnd));
+
+
+    }
+}
+
+class IoManager {
+    String input(String text) {
+        Scanner scanner = new Scanner(System.in);
+        String inputValue = scanner.nextLine();
+
+        return inputValue;
+    }
+}
+
+class Calculator {
+    int SumFromTo(int start, int end) {
         int sum = 0;
-        for(int i = 1; i <= 10; i++) {
+        for(int i = start; i <= end; i++) {
             sum += i;
         }
 
-        System.out.println(sum);
+       return sum;
     }
 }
+
+// data를 다루는 클래스를 DTO
+// 한 기능을 다루는 클래스 Component?
