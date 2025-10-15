e248d069a1afd44b1372fcd55add25962d63f89f
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-10-15 15:12:36 +0900

    JVM

--- P1015_1/src/P7/App.java
+++ P1015_1/src/P7/App.java
@@ -1,8 +1,12 @@
 package P7;
 
 public class App {
+
+    // JVM 은 Class main을 찾아서 그걸 로딩시켰을 것이다.
     public static void main(String[] args) {
         Test.qqqq();
+
+        System.out.println("!@#$%^&*(\\'``<>?:;'");
     }
 }
 
@@ -14,4 +18,6 @@ class Test {
     static void qqqq() {
         System.out.println("qqqq");
     }
+
+    
 }
---
+++ P1015_1/src/P8/App.java
@@ -0,0 +1,18 @@
+package P8;
+
+public class App {
+    public static void main(String[] args) {
+        Test t1 = new Test();
+        t1.a = 10;
+    }
+}
+
+class Test {
+    // 일반적으로 public, priate을 쓴다.
+    public int a;   // 중요
+    protected int b;
+    int c;
+    private int d;  // 중요
+    
+
+}
