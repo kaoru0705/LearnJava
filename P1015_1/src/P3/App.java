06a0a20b5a76395257e1ca5f62aaf324675b382f
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-10-15 11:44:44 +0900

    class 역할 method역할 이름 정하기 연습1

--- P1015_1/src/P3/App.java
+++ P1015_1/src/P3/App.java
@@ -4,34 +4,69 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        
+        IoManager ioManager = new IoManager();
         Calculator calculator = new Calculator();
+
+        String value = ioManager.input("값 입력 > ");
+        ioManager.output("입력된 값 = " + value);
+
 
         int inputStart = 77;
         int inputEnd = 1500;
         System.out.println(calculator.SumFromTo(inputStart, inputEnd));
+
+        
 
 
     }
 }
 
 class IoManager {
+    Scanner scanner = new Scanner(System.in);
+
     String input(String text) {
-        Scanner scanner = new Scanner(System.in);
+        System.out.print(text);
         String inputValue = scanner.nextLine();
 
         return inputValue;
     }
+
+    void output(String text) {
+        System.out.println(text);
+    }
 }
+
+// class Calculator {
+//     int sum = 0;
+//     int SumFromTo(int start, int end) {
+//         sum = 0; // 초기화는 일반적으로 생성자를 통해서만 하는 게 낫다.
+
+//         for(int i = start; i <= end; i++) {
+//             accumulate(i);
+//         }
+    
+//        return sum;
+//     }
+
+//     void accumulate(int i) {
+//         sum += i;
+//     }
+// }
 
 class Calculator {
     int SumFromTo(int start, int end) {
+        
         int sum = 0;
         for(int i = start; i <= end; i++) {
-            sum += i;
+            sum = this.accumulate(sum, i); // this 생략 가능
         }
-
+    
        return sum;
+    }
+
+    int accumulate(int sum, int i) {
+        sum += i;
+        return sum;
     }
 }
 
