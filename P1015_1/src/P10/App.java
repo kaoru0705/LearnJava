79ddc86b83fe4fc766283289d3dd7dce6354efe2
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-10-15 16:26:19 +0900

    DTO(Data Transfer Object)
    데이터를 전송하기 위한 객체
    getter setter 를 만들었다.

--- P1015_1/src/P10/App.java
+++ P1015_1/src/P10/App.java
@@ -2,10 +2,41 @@ package P10;
 
 public class App {
     public static void main(String[] args) {
-        
+        StudentsDto s1 = new StudentsDto();
+        s1.setName("철수");
+        System.out.println(s1.getName());
     }
 }
 
+// Dto = 기능 없음. 데이터 묶는 용도의 클래스
 class StudentsDto {
+    private String name;
+    private int age;
+    private int score;
+    // setter - write 권한
+    // getter - read 권한
     
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
 }
