e43cd07a26e8196a5723ee2af1b3d3ad1527e446
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-10-15 15:19:59 +0900

    access speciafier

--- P1015_1/src/P8/App.java
+++ P1015_1/src/P8/App.java
@@ -7,12 +7,18 @@ public class App {
     }
 }
 
+// 접근 제한자 - 캡슐화, 정보의 은닉
 class Test {
     // 일반적으로 public, priate을 쓴다.
-    public int a;   // 중요
-    protected int b;
-    int c;
-    private int d;  // 중요
+    public int a;   // 어디서나 접근 가능
+    private int d;  // 중요 같은 클래스 내에서만 접근 가능
+    protected int b; // 같은 패키지 내부 혹은 다른 패키지의 자식클래스는 접근 가능
+    int c; // 같은 패키지 내부에서만 가능
+
+    public void qqqq() {
+        a = 10;
+        d = 10;
+    }
     
 
 }
