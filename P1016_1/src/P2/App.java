c3015f7121b3e30dff65b4ef264d219a2a4e8146
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-10-16 11:25:28 +0900

    추상화

---
+++ P1016_1/src/P2/App.java
@@ -0,0 +1,31 @@
+package P2;
+
+public class App {
+    public static void main(String[] args) {
+        new 식사().식사하다();
+    }
+}
+
+ class 식사 {
+
+    public void 식사하다() {
+        재료를준비하다();
+        요리를한다();
+        음식을먹는다();
+    }
+    
+    private void 재료를준비하다() {
+        마트갈준비를하다();
+        마트에가다();
+        재료를구입하다();
+        마트에서돌아온다();
+    }
+
+    private void 마트갈준비를하다() {}
+    private void 마트에가다() {}
+    private void 재료를구입하다() {};
+    private void 마트에서돌아온다() {};
+
+    private void 요리를한다() {}
+    private void 음식을먹는다() {}
+ }
---
+++ P1016_1/src/P3/App.java
@@ -0,0 +1,9 @@
+package P3;
+
+public class App {
+    public static void main(String[] args) {
+        // 클래스 설계 원칙
+        // 캡슐화(정보의 은닉), 응집도를 높인다.
+        System.out.println("");
+    }
+}
