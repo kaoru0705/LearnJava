c1ddad3b52650d12f377706b5fd0818b41f7d09a
kaoru0705 <kaoru9875@gmail.com>
2025-10-17 22:48:07 +0900

    studentDto 구현

--- StudentManagerKdh/src/kdh/stm/App.java
+++ StudentManagerKdh/src/kdh/stm/App.java
@@ -1,6 +1,8 @@
 package kdh.stm;
 
 import java.util.*;
+
+import kdh.stm.dto.StudentDto;
 import kdh.stm.util.IoManager;
 
 public class App {
@@ -13,9 +15,7 @@ public class App {
         IoManager.println("          개발자: 강동훈    ");
         IoManager.println("=============================");
 
-        List<String> nameList = new ArrayList<>();
-        List<Integer> ageList = new ArrayList<>();
-        List<Integer> scoreList = new ArrayList<>();
+        List<StudentDto> list = new ArrayList<>();
 
         int count = 0;
 
@@ -38,20 +38,23 @@ public class App {
             if (command.equals("1")) {
                 IoManager.println("[테스트 로그] 학생 등록 로직 수행");
 
-                String name = IoManager.strInput("이름 입력 > ");
+                StudentDto studentDto = new StudentDto();
 
-                nameList.add(name);
+                studentDto.setName(IoManager.strInput("이름 입력 > "));
 
-                ageList.add(IoManager.integerInput("나이 입력 > "));
+                studentDto.setAge(IoManager.integerInput("나이 입력 > "));
 
-                scoreList.add(IoManager.integerInput("점수 입력 > "));
+                studentDto.setScore(IoManager.integerInput("점수 입력 > "));
 
+                list.add(studentDto);
                 count++;
+
             } else if (command.equals("2")) {
                 System.out.println("[테스트 로그] 학생 목록 로직 수행");
                 for (int index = 0; index < count; index++) {
-                    System.out.println("학생의 이름: " + nameList.get(index) + " 나이: " + ageList.get(index) + " 점수: "
-                            + scoreList.get(index));
+                    System.out.println("학생의 이름: " + list.get(index).getName()
+                            + " 나이: " + list.get(index).getAge()
+                            + " 점수: " + list.get(index).getScore());
                 }
             } else if (command.equals("3")) {
                 IoManager.println("[테스트 로그] 학생 검색 로직");
@@ -59,9 +62,10 @@ public class App {
                 int searchCount = 0;
                 IoManager.println("이름에 " + searchName + "이(가) 포함되는 학생은 ");
                 for (int searchIndex = 0; searchIndex < count; searchIndex++) {
-                    if (nameList.get(searchIndex).contains(searchName)) {
-                        IoManager.println("\t이름: " + nameList.get(searchIndex) + " 나이: "
-                                + ageList.get(searchIndex) + " 점수: " + scoreList.get(searchIndex));
+                    if (list.get(searchIndex).getName().contains(searchName)) {
+                        IoManager.println("\t이름: " + list.get(searchIndex).getName()
+                                + " 나이: " + list.get(searchIndex).getAge()
+                                + " 점수: " + list.get(searchIndex).getScore());
                         searchCount++;
                     }
                 }
@@ -76,7 +80,7 @@ public class App {
                 boolean isDelete = false;
                 boolean isCancel = false;
                 for (int nameIndex = 0; nameIndex < count; nameIndex++) {
-                    if (nameList.get(nameIndex).equals(deletedName)) {
+                    if (list.get(nameIndex).getName().equals(deletedName)) {
                         String guest = IoManager.YesOrNoInput();
 
                         if (guest.toUpperCase().equals("N") || guest.toUpperCase().equals("NO")) {
@@ -86,10 +90,8 @@ public class App {
                         }
                     }
 
-                    if (nameList.get(nameIndex).equals(deletedName)) {
-                        nameList.remove(nameIndex);
-                        ageList.remove(nameIndex);
-                        scoreList.remove(nameIndex);
+                    if (list.get(nameIndex).getName().equals(deletedName)) {
+                        list.remove(nameIndex);
                         nameIndex--;
                         IoManager.println("삭제되었습니다.");
                         count--;
