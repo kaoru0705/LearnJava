3239a477641ebab576c67a384948de4e1160588a
kaoru0705 <kaoru9875@gmail.com>
2025-10-17 21:49:56 +0900

    inputoutput logic

--- StudentManagerKdh/src/kdh/stm/App.java
+++ StudentManagerKdh/src/kdh/stm/App.java
@@ -1,19 +1,17 @@
 package kdh.stm;
 
 import java.util.*;
+import kdh.stm.util.IoManager;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // 주제: 학생 정보 관리 프로그램
         // 기능: 학생 정보 등록, 목록, 삭제, 수정...
-        Scanner scanner = new Scanner(System.in);
 
-        // 환영 인사
-        // sout tab으로 System.out.println()을 만들 수 있다.
-        System.out.println("=============================");
-        System.out.println("     학생 관리 프로그램 v1     ");
-        System.out.println("          개발자: 강동훈    ");
-        System.out.println("=============================");
+        IoManager.println("=============================");
+        IoManager.println("     학생 관리 프로그램 v2     ");
+        IoManager.println("          개발자: 강동훈    ");
+        IoManager.println("=============================");
 
         List<String> nameList = new ArrayList<>();
         List<Integer> ageList = new ArrayList<>();
@@ -22,72 +20,31 @@ public class App {
         int count = 0;
 
         while (true) {
-            System.out.println("****메뉴****");
-            System.out.println("1. 학생 정보 등록");
-            System.out.println("2. 학생 정보 목록");
-            System.out.println("3. 학생 정보 검색");
-            System.out.println("4. 학생 정보 삭제");
-            System.out.println("5. 학생 정보 수정");
-            System.out.println("0. 프로그램 종료");
+            IoManager.println("****메뉴****");
+            IoManager.println("1. 학생 정보 등록");
+            IoManager.println("2. 학생 정보 목록");
+            IoManager.println("3. 학생 정보 검색");
+            IoManager.println("4. 학생 정보 삭제");
+            IoManager.println("5. 학생 정보 수정");
+            IoManager.println("0. 프로그램 종료");
 
-            System.out.print("명령어 입력 >");
-            String command = scanner.nextLine();
+            String command = IoManager.input("명령어 입력 >");
 
             if (command.equals("0")) {
-                System.out.println("학생 관리 프로그램을 종료합니다.");
+                IoManager.println("학생 관리 프로그램을 종료합니다.");
                 break;
             }
 
             if (command.equals("1")) {
-                System.out.println("[테스트 로그] 학생 등록 로직 수행");
+                IoManager.println("[테스트 로그] 학생 등록 로직 수행");
 
-                System.out.print("이름 입력 > ");
-                String name = scanner.nextLine();
-
-                while (name.equals("")) {
-                    System.out.print("이름에 제대로된 문자열을 입력하세요. > ");
-                    name = scanner.nextLine();
-                }
+                String name = IoManager.strInput("이름 입력 > ");
 
                 nameList.add(name);
 
-                System.out.print("나이 입력 > ");
+                ageList.add(IoManager.integerInput("나이 입력 > "));
 
-                // String strAge = null;
-                // while(true)
-                // {
-                // boolean isIntger = true;
-
-                // strAge = scanner.nextLine();
-                // for (int strIndex = 0 ; strIndex < strAge.length(); strIndex++) {
-                // if(strAge.charAt(strIndex) < '0' || strAge.charAt(strIndex) > '9') {
-                // isIntger = false;
-                // System.out.print("나이를 다시 입력하세요. > ");
-                // break;
-                // }
-                // }
-
-                // if(strAge.length() != 0 && isIntger == true) {
-                // break;
-                // }
-                // }
-                // ageList[count] = Integer.parseInt(strAge);
-
-                // String strAge = scanner.nextLine();
-                while (!scanner.hasNextInt()) {
-                    System.out.println("숫자를 입력하세요.");
-                    System.out.print("나이 입력 > ");
-                    scanner.nextLine();
-                }
-                ageList.add(Integer.parseInt(scanner.nextLine()));
-
-                System.out.print("점수 입력 > ");
-                while (!scanner.hasNextInt()) {
-                    System.out.println("숫자를 입력하세요.");
-                    System.out.print("점수 입력 > ");
-                    scanner.nextLine();
-                }
-                scoreList.add(Integer.parseInt(scanner.nextLine()));
+                scoreList.add(IoManager.integerInput("점수 입력 > "));
 
                 count++;
             } else if (command.equals("2")) {
@@ -97,39 +54,33 @@ public class App {
                             + scoreList.get(index));
                 }
             } else if (command.equals("3")) {
-                System.out.println("[테스트 로그] 학생 검색 로직");
-                System.out.print("검색할 학생의 이름: > ");
-                String searchName = scanner.nextLine();
+                IoManager.println("[테스트 로그] 학생 검색 로직");
+                String searchName = IoManager.strInput("검색할 학생의 이름: > ");
                 int searchCount = 0;
-                System.out.println("이름에 " + searchName + "이 포함되는 학생은 ");
+                IoManager.println("이름에 " + searchName + "이(가) 포함되는 학생은 ");
                 for (int searchIndex = 0; searchIndex < count; searchIndex++) {
                     if (nameList.get(searchIndex).contains(searchName)) {
-                        System.out.println("\t이름: " + nameList.get(searchIndex) + " 나이: "
+                        IoManager.println("\t이름: " + nameList.get(searchIndex) + " 나이: "
                                 + ageList.get(searchIndex) + " 점수: " + scoreList.get(searchIndex));
                         searchCount++;
                     }
                 }
                 if (searchCount == 0) {
-                    System.out.println("등록된 학생이 없습니다.");
+                    IoManager.println("등록된 학생이 없습니다.");
                 } else {
-                    System.out.println("으로 총 " + searchCount + "명 있습니다.");
+                    IoManager.println("으로 총 " + searchCount + "명 있습니다.");
                 }
             } else if (command.equals("4")) {
-                System.out.println("[테스트 로그] 학생 삭제 로직");
-                System.out.print("삭제할 이름은? > ");
-                String deletedName = scanner.nextLine();
+                IoManager.println("[테스트 로그] 학생 삭제 로직");
+                String deletedName = IoManager.strInput("삭제할 이름은? > ");
                 boolean isDelete = false;
                 boolean isCancel = false;
                 for (int nameIndex = 0; nameIndex < count; nameIndex++) {
                     if (nameList.get(nameIndex).equals(deletedName)) {
-                        System.out.print("삭제할 이름을 찾았습니다.\n마지막으로 묻습니다. 정말 삭제하시겠습니까? (Y/N) > ");
-                        String guest = scanner.nextLine();
-                        while (!guest.equals("n") && !guest.equals("N") && !guest.equals("y") && !guest.equals("Y")) {
-                            System.out.print("(Y/N) 중 하나를 입력하세요. > ");
-                            guest = scanner.nextLine();
-                        }
-                        if (guest.equals("n") || guest.equals("N")) {
-                            System.out.println("삭제하지 않기로 했습니다.");
+                        String guest = IoManager.YesOrNoInput();
+
+                        if (guest.toUpperCase().equals("N") || guest.toUpperCase().equals("NO")) {
+                            IoManager.println("삭제하지 않기로 했습니다.");
                             isCancel = true;
                             break;
                         }
@@ -140,27 +91,24 @@ public class App {
                         ageList.remove(nameIndex);
                         scoreList.remove(nameIndex);
                         nameIndex--;
-                        System.out.println("삭제되었습니다.");
+                        IoManager.println("삭제되었습니다.");
                         count--;
                         isDelete = true;
                     }
                 }
 
                 if (!isDelete && !isCancel) {
-                    System.out.println("존재하지 않는 이름입니다.");
+                    IoManager.println("존재하지 않는 이름입니다.");
                 }
             } else if (command.equals("5")) {
-                System.out.println("[테스트 로그] 학생 정보 수정 로직");
+                IoManager.println("[테스트 로그] 학생 정보 수정 로직");
             } else {
-                System.out.println("번호를 잘못 입력하셨습니다.");
-                System.out.println("다시 입력 바랍니다.");
+                IoManager.println("번호를 잘못 입력하셨습니다.");
+                IoManager.println("다시 입력 바랍니다.");
                 continue;
             }
 
-            System.out.println("계속 입력하시려면 enter를 눌러주세요.");
-            scanner.nextLine();
+            IoManager.input("계속 입력하시려면 enter를 눌러주세요.");
         }
-
-        scanner.close();
     }
 }
---
+++ StudentManagerKdh/src/kdh/stm/util/IoManager.java
@@ -0,0 +1,78 @@
+package kdh.stm.util;
+
+import java.util.Scanner;
+
+public class IoManager {
+    private static Scanner scanner = new Scanner(System.in);
+
+    private IoManager() {
+    };
+
+    public static String input(String text) {
+        print(text);
+
+        return scanner.nextLine();
+    }
+
+    public static String strInput(String text) {
+        print(text);
+
+        return requireNonEmpty(text);
+    }
+
+    public static int integerInput(String text) {
+        print(text);
+
+        return Integer.parseInt(requireIntegerString(text));
+    }
+
+    public static String YesOrNoInput() {
+        print("삭제할 이름을 찾았습니다.\n마지막으로 묻습니다. 정말 삭제하시겠습니까? (Y/N) > ");
+
+        return requireYesOrNoString();
+    }
+
+    public static void print(String text) {
+        System.out.print(text);
+    }
+
+    public static void println(String text) {
+        System.out.println(text);
+    }
+
+    private static String requireNonEmpty(String text) {
+
+        String strInput = scanner.nextLine();
+
+        while (strInput.equals("")) {
+            println("빈 문자열입니다.");
+            print(text);
+            strInput = scanner.nextLine();
+        }
+
+        return strInput;
+    }
+
+    private static String requireIntegerString(String text) {
+
+        while (!scanner.hasNextInt()) {
+            println("숫자가 아닙니다.");
+            print(text);
+            scanner.nextLine();
+        }
+
+        return scanner.nextLine();
+    }
+
+    private static String requireYesOrNoString() {
+        String guest = scanner.nextLine();
+        while (!guest.toUpperCase().equals("N") && !guest.toUpperCase().equals("Y")
+                && !guest.toUpperCase().equals("YES") && !guest.toUpperCase().equals("NO")) {
+            IoManager.print("(Y/N) 중 하나를 입력하세요. > ");
+            guest = scanner.nextLine();
+        }
+
+        return guest;
+    }
+
+}
