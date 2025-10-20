8667d9b9752629757c1d9cb9bd378804d76222a1
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-10-20 14:22:26 +0900

    polymorphism
    instanceof operator

--- P1020_1/src/p8/App.java
+++ P1020_1/src/p8/App.java
@@ -2,6 +2,38 @@ package p8;
 
 public class App {
     public static void main(String[] args) {
+        // 다형성 polymorphism
+        // 특정 타입의 변수는 특정타입을 상속받는 모든 형태의 타입을 변수로 받을 수 있다.
+        AAA a1 = new BBB();
+        a1.test();
+        // a1.b1 = 10; // compile error 자손의 기능을 사용 못 함
+
+        // instanceof opeartor
+        if(a1 instanceof BBB) {
+            BBB tempB = (BBB)a1; // 캐스팅 가능. 상속 관계일 경우에만
+            tempB.test();
+        }
+
+
+    
         
+    }
+}
+
+class AAA {
+    int a1;
+    int a2;
+
+    void test() {
+        System.out.println("AAA");
+    }
+}
+
+class BBB extends AAA {
+    int b1;
+    int b2;
+
+    void test() {
+        System.out.println("BBB");
     }
 }
