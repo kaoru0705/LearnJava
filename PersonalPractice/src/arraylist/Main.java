c825d8009ab7cc3a5a0c3c899eda77335daf7eb0
kaoru0705 <kaoru9875@gmail.com>
2025-10-21 21:07:20 +0900

    LinkedList ArrayList

--- PersonalPractice/src/arraylist/Main.java
+++ PersonalPractice/src/arraylist/Main.java
@@ -6,6 +6,9 @@ import java.util.Iterator;
 public class Main {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
+        // 선형 리스트 연결 리스트를 배열기반으로 만듦
+        // 내부적으로는 처음 배열크기를 넉넉하게 잡고 size()는 추가한 개수만큼만 반환
+        // 당연히 특정 값을 제거하거나 삽입하면 느리다. 크기가 너무 커져도 그렇다.
 
         list.add("one");
         list.add("two");
--- PersonalPractice/src/linkedlist/stack/App.java
+++ PersonalPractice/src/linkedlist/stack/App.java
@@ -3,6 +3,8 @@ import java.util.LinkedList;
 import java.util.NoSuchElementException;
 public class App {
     public static void main(String[] args) {
+        // 이중 연결 리스트다. 그러니 큐 스택 둘 다 가능함. 당연히 데이터 접근속도는 배열 리스트에 비해 느릴 수밖에 없다. 삽입혹은 삭제는 빠르다.
+        // pointer로 그부분만 바꿔주면 되니
         LinkedList<String> stack = new LinkedList<>();
         stack.push("one");
         stack.push("two");
