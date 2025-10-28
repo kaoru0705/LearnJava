d3c68de20b84cea75d2ae28c3b8b3a156942e7c0
kaoru0705 <kaoru9875@gmail.com>
2025-10-28 17:30:07 +0900

    HashMap

--- P1028_1/src/p6/App.java
+++ P1028_1/src/p6/App.java
@@ -11,6 +11,13 @@ public class App {
         map.put("철수", new StudentDto("철수"));
 
         StudentDto s = map.get("한조");
+
+        // 키값의 hashCode를 이용해서 배열 인덱스를 결정한다. 따라서 O(1)
+        // 만일 인덱스가 같을 경우(충돌할 경우) 링크드 리스트로 관리 최악이면 O(n) 
+        // java8부터는 노드가 8개 이상일 경우 Tree로 관리한다고 한다. O(log n)
+        // sha hash 알고리즘 LSH 알고리즘
+        System.out.println("철수".hashCode() % 16);
+        System.out.println(Integer.valueOf(42).hashCode());
     }
 }
 
---
+++ P1028_1/src/p7/App.java
@@ -0,0 +1,20 @@
+package p7;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String text = "안녕하세요!!";
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        byte[] digest = md.digest(text.getBytes(StandardCharsets.UTF_8));
+
+        // 보기 쉽게 문자열로 변환
+        StringBuilder stringBuilder = new StringBuilder();
+        for(byte b : digest) {
+            stringBuilder.append(String.format("%02x", b));
+        }
+        System.out.println(stringBuilder.toString());
+
+    }
+}
