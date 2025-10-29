d669402b82ad231bd3471369736bff0c55ef3ea3
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-10-29 12:24:07 +0900

    interface 사용하기

--- StudentManagerInterface/src/com/joongang/stm/service/Service.java
+++ StudentManagerInterface/src/com/joongang/stm/service/Service.java
@@ -1,12 +1,13 @@
 package com.joongang.stm.service;
 
-import java.util.Arrays;
-
 import com.joongang.stm.dto.StudentDto;
+import com.joongang.stm.menu.ActionCommand;
+import com.joongang.stm.menu.Menu;
 import com.joongang.stm.util.IoManager;
 import com.joongang.stm.repository.Repository;
 
 import java.util.List;
+
 // 컴포넌트
 // 역할: 비즈니스로직(코어로직) 담당
 public class Service {
@@ -14,13 +15,14 @@ public class Service {
     private Repository repository = new Repository();
 
 
-    public void addStudent() {
+    public void registerStudent() {
         IoManager.println("===== 학생 정보 입력 시작 =====");
         String name = IoManager.input("이름 입력 > ");
+        String id = IoManager.input("학번 입력 > ");
         int age = IoManager.intgerInput("나이 입력 > ");
         int score = IoManager.intgerInput("점수 입력 > ");
 
-        StudentDto studentDto = new StudentDto(name, age, score);
+        StudentDto studentDto = new StudentDto(name, id, age, score);
 
         repository.save(studentDto);
 
@@ -90,5 +92,34 @@ public class Service {
         IoManager.println("평균: " + avg);
 
         IoManager.println("============================");
+    }
+
+    public void add() {
+
+        ActionCommand register = new ActionCommand("학생 등록", this::registerStudent);
+        ActionCommand search = new ActionCommand("학생 정보 검색", this::searchStudent);
+
+        Menu studentMenu = new Menu("학생 관리");
+        studentMenu.add(register);
+        studentMenu.add(search);
+
+        Menu mainMenu = new Menu("학생 관리 시스템 메인");
+        mainMenu.add(studentMenu);
+
+        System.out.println("== 학생 관리 프로그램 시작 ==");
+        mainMenu.execute(); 
+    }
+
+    public void welcome() {
+        IoManager.println("********************************");
+        IoManager.println("         학생 관리 프로그램        ");
+        IoManager.println("          version2              ");
+        IoManager.println("          2025.10.17            ");
+        IoManager.println("********************************");
+    }
+
+    public void bye() {
+        IoManager.println("프로그램이 종료됩니다.");
+        IoManager.println("이용해주셔서 감사합니다.");
     }
 }
