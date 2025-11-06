c77f214d47c39fc21bb76a6ccc8cc8fc4b898800
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-11-06 16:26:53 +0900

    boj2468 시작

---
+++ Algorithms/src/boj2468/Main.java
@@ -0,0 +1,10 @@
+package boj2468;
+
+public class Main {
+    private static boolean isVisited[][];
+    private static int arr[][];
+
+    public static void main(String[] args) {
+        
+    }
+}
