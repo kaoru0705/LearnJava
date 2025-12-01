1693571066b77e48f69da6e6d9625004606ca653
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-12-01 17:39:58 +0900

    boj9005 ing

---
+++ Algorithms/src/boj9095/Main.java
@@ -0,0 +1,16 @@
+package boj9095;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int t = Integer.parseInt(st.nextToken());
+        // arr-3 + arr-2 + arr-1 = arr
+    }
+}
