0a6cc2c2b69134118303a9995c066b203c2dbb1c
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-12-08 11:02:28 +0900

    Create Main.java

---
+++ Algorithms/src/boj2579/Main.java
@@ -0,0 +1,15 @@
+package boj2579;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        
+    }
+}
